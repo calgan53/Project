e61895f91885a63167d95f521e13b5362f137431
calgan53 <127695781+calgan53@users.noreply.github.com>
2025-12-03 20:46:43 -0500

    added method of isPostalCodeValid

--- Address.java
+++ Address.java
@@ -1,1 +1,38 @@
+package org.example;
 
+public class Address {
+    int streetNo;
+    String street;
+    String city;
+    Province province; // enum, contains abbriviations of all provinces in Canada
+    String postalCode;
+
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null) {
+            return false;
+        }
+        if (postalCode.length() != 6) {
+            return false;
+        }
+        for (int i = 0; i < 6; i++) {
+            char ch = postalCode.charAt(i);
+
+            if (i % 2 == 0) {
+                if (!Character.isLetter(ch)) {
+                    return false;
+                }
+            } else {
+                if (!Character.isDigit(ch)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+    public enum Province {
+        AB, BC, MB, NB, NL, NS, ON, PE, QC, SK
+    }
+}
